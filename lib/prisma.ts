b352b35d19cd82afd08c5d65a531d3f85106d46b
Daniele <daniele.tortora@tundra.com>
2022-05-07 16:49:33 +0200

    prisma connection error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+/**
+ * Fix Connection Error Prisma
+ * https://flaviocopes.com/nextjs-fix-prismaclient-unable-run-browser/
+ */
+let db;
 
-export const prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  db = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  db = global.prisma;
+}
+export const prisma = db as PrismaClient;
